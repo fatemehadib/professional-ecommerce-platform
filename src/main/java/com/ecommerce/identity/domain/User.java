675dfb0609e25a6291db8233bf90c1d0784faa63
Fatemeh Sotoodeh <F.Sotoodeh@DOTIN.IR>
2025-08-13 15:29:36 +0330

    feat: add pom file and spring framework base files

---
+++ src/main/java/com/ecommerce/identity/domain/User.java
@@ -0,0 +1,34 @@
+package com.ecommerce.identity.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}
